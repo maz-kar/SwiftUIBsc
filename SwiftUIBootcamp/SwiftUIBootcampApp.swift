6718a1fe79f2213bd33e494ab808e98a477b97b4
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-07-18 16:49:41 +0200

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Maziar Layeghkar on 18.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
