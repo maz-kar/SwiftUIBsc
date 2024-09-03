3994b9775a65dda1bca989dfb86d853f6b3dc4e0
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-09-03 14:35:26 +0200

    addFruit method added.

--- SwiftUIBootcamp/CoreData.swift
+++ SwiftUIBootcamp/CoreData.swift
@@ -11,6 +11,7 @@ import CoreData
 class CoreDataViewModel: ObservableObject {
     let container: NSPersistentContainer
     @Published var savedEntities: [FruitEntity] = []
+    @State var textFieldText: String = ""
     
     init() {
         container = NSPersistentContainer(name: "FruitsContainer")
@@ -33,13 +34,55 @@ class CoreDataViewModel: ObservableObject {
         }
     }
     
+    func addFruit(text: String) {
+        let newFruit = FruitEntity(context: container.viewContext)
+        newFruit.name = text
+        
+        saveData()
+    }
+    
+    func saveData() {
+        do {
+            try container.viewContext.save()
+            //Every time that we tap the save, we need to update the @Published value and inside of this fetchRequest we are doing it.
+            fetchRequest()
+        } catch let error {
+            print("Error saving: \(error)")
+        }
+    }
+    
 }
 
 struct CoreData: View {
     @StateObject var vm = CoreDataViewModel()
+    let backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello")
+//        VStack(spacing: 15) {
+//            TextField("Add your Fruit here...", text: vm.$textFieldText)
+//                .font(.headline)
+//                .frame(maxWidth: .infinity)
+//                .frame(height: 55)
+//                .padding(.horizontal)
+//                .background(Color(backgroundColor))
+//                .clipShape(RoundedRectangle(cornerRadius: 10))
+//            
+//            Button(action: {
+//                vm.addFruit(text: vm.textFieldText)
+//                print(vm.textFieldText)
+//            }, label: {
+//                Text("Submit")
+//                    .font(.headline)
+//                    .foregroundStyle(Color.white)
+//                    .frame(maxWidth: .infinity)
+//                    .frame(height: 55)
+//                    .padding(.horizontal)
+//                    .background(Color.blue)
+//                    .clipShape(RoundedRectangle(cornerRadius: 10))
+//            })
+//        }
+//        .padding()
     }
 }
 
